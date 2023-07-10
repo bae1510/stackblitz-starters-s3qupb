7cf04440839f4da99e7d82c1b8e4a10ea904d567
bae1510 <60731764+bae1510@users.noreply.github.com>
2023-07-10 15:52:49 +0530

    Todo with UseReducer and Context hook

--- src/App.js
+++ src/App.js
@@ -1,66 +1,110 @@
-import React from 'react';
-import { useState } from 'react';
-export default function Calculator() {
-  const [num1, setNum1] = useState();
-  const [num2, setNum2] = useState();
-  const [result, setResult] = useState();
+import React, { createContext, useContext, useReducer } from 'react';
 
-  function handleValue1(e) {
-    setNum1(e.target.value);
-  }
-  function handleValue2(e) {
-    setNum2(e.target.value);
-  }
+const UserContext = createContext();
 
-  function handleAdd() {
-    const sum = Number(num1) + Number(num2);
-    setResult(sum);
-    setNum1('');
-    setNum2('');
+const initialState = {
+  users: [],
+};
+
+const ADD_USER = 'ADD_USER';
+const UPDATE_USER = 'UPDATE_USER';
+const DELETE_USER = 'DELETE_USER';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ADD_USER:
+      return { ...state, users: [...state.users, action.payload] };
+    case UPDATE_USER:
+      return {
+        ...state,
+        users: state.users.map((user) =>
+          user.id === action.payload.id ? action.payload : user
+        ),
+      };
+    case DELETE_USER:
+      return {
+        ...state,
+        users: state.users.filter((user) => user.id !== action.payload),
+      };
+    default:
+      throw new Error('Unsupported action type');
   }
-  function handleSub() {
-    const sub = Number(num1) - Number(num2);
-    setResult(sub);
-    setNum1('');
-    setNum2('');
-  }
-  function handleMul() {
-    const mul = Number(num1) * Number(num2);
-    setResult(mul);
-    setNum1('');
-    setNum2('');
-  }
-  function handleDiv() {
-    const div = Number(num1) / Number(num2);
-    setResult(div);
-    setNum1('');
-    setNum2('');
-  }
+};
+
+const UserProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <UserContext.Provider value={{ state, dispatch }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+const useUser = () => useContext(UserContext);
+
+const UserList = () => {
+  const { state, dispatch } = useUser();
 
   return (
     <div>
-      <h3>Calculator</h3>
-      <input
-        type="number"
-        placeholder="Enter a value"
-        value={num1}
-        onChange={handleValue1}
-      />
-      <br />
-      <br />
-      <input
-        type="number"
-        placeholder="Enter a value "
-        value={num2}
-        onChange={handleValue2}
-      />
-      <br />
-      <br />
-      <button onClick={handleAdd}>Addition</button>
-      <button onClick={handleSub}>Substraction</button>
-      <button onClick={handleMul}>Multiplication</button>
-      <button onClick={handleDiv}>Division</button>
-      <h2>Result:{result}</h2>
+      <h2>User List</h2>
+      {state.users.length === 0 ? (
+        <p>No users found.</p>
+      ) : (
+        <ul>
+          {state.users.map((user) => (
+            <li key={user.id}>
+              {user.name} - {user.email}
+              <button
+                onClick={() =>
+                  dispatch({ type: DELETE_USER, payload: user.id })
+                }
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-}
+};
+
+const UserForm = () => {
+  const { dispatch } = useUser();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const id = Date.now().toString();
+    if (name && email) {
+      const user = { id, name, email };
+      dispatch({ type: ADD_USER, payload: user });
+      e.target.reset();
+    }
+  };
+
+  return (
+    <div>
+      <h2>Add User</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Name" required />
+        <input type="email" name="email" placeholder="Email" required />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <UserProvider>
+      <UserList />
+      <UserForm />
+    </UserProvider>
+  );
+};
+
+export default App;
